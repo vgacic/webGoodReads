1c5e05514f3b3c235b1fc93cb56f080e056e00e8
vgacic <vgacic02@gmail.com>
2023-06-14 17:08:02 +0200

    Inicijalizacija frontend servera

--- web/src/main/java/com/tim22/web/repository/ZahtevZaAktivacijuNalogaAutoraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tim22.web.repository;
-
-import com.tim22.web.entity.ZahtevZaAktivacijuNalogaAutora;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZahtevZaAktivacijuNalogaAutoraRepository extends JpaRepository<ZahtevZaAktivacijuNalogaAutora, Long> {
-}
